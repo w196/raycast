e4cbf92667cbe1e097337a6fc4279675cee3960b
w196 <no-0082@protonmail.ch>
2022-12-29 17:11:35 -0500

    removed bulid dir

--- build/external/SDL/include/SDL_revision.h
+++
@@ -1,8 +0,0 @@
-/* #undef SDL_VENDOR_INFO */
-#define SDL_REVISION_NUMBER 0
-
-#ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-release-2.26.0-121-g29a4c5ebc (" SDL_VENDOR_INFO ")"
-#else
-#define SDL_REVISION "SDL-release-2.26.0-121-g29a4c5ebc"
-#endif
--- build/external/SDL_image/CMakeFiles/SDL2_image.dir/IMG_tga.c.o.d
+++
@@ -1,2 +0,0 @@
-external/SDL_image/CMakeFiles/SDL2_image.dir/IMG_tga.c.o: \
-  /Volumes/T7/code/raycast/external/SDL_image/IMG_tga.c
